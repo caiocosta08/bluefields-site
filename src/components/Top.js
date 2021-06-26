e36e1079e919e7aeda8ae4d767fcc08654fdcdd0
Victor Sales <victsales@gmail.com>
2021-06-26 12:31:37 -0300

    styles: fix responsivity at corporative landing page

--- src/components/Top.js
+++ src/components/Top.js
@@ -8,23 +8,13 @@ function Top({lightBg,topLine,lightText,lightTextDesc,headline,description,butto
     return (
         <div className={`${lightBg ? 'home__hero-section fundoImage' : 'home__hero-section darkBg'} ${corporative ? 'fundoCorporative' : ''}`}>
             <div className="container">
-                <div className="row home__hero-row" style={{ display:'flex',
-                flexDirection: imgStart === 'start' ? 'row-reverse' : 'row'}}>
-                    <div className="col">
-                        <div className="home__hero-text-wrapper">
-                            <div className="top-line">{topLine}</div>
-                            <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
-                            <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}>{description}</p>
-                            <Link to={caminho}>
-                                <Button buttonSize='btn--wide' buttonColor='blue'>{buttonLabel}</Button>
-                            </Link>
-                        </div>
-                    </div>
-                    {/* <div className="col">
-                        <div className="home__hero-img-wrapper">
-                            <img src={img} alt={alt} className="home__hero-img"/> 
-                        </div>
-                    </div> */}
+                <div className="home__hero-text-wrapper">
+                    <div className="top-line">{topLine}</div>
+                    <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
+                    <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}>{description}</p>
+                    <Link to={caminho}>
+                        <Button buttonSize='btn--wide' buttonColor='blue'>{buttonLabel}</Button>
+                    </Link>
                 </div>
             </div>
         </div>
